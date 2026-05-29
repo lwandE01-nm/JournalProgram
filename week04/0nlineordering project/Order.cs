c20f746287274410c8307e4eb0f390ab558fa172
lwandE01-nm <lwandE01@icloud.com>
2026-05-29 21:19:04 +0200

    Create Order.cs

---
+++ week04/0nlineordering project/Order.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Text;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(List<Product> products, Customer customer)
+    {
+        _products = products;
+        _customer = customer;
+    }
+
+    public double CalculateTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        // Shipping cost
+        if (_customer.LivesInUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        StringBuilder label = new StringBuilder();
+
+        foreach (Product product in _products)
+        {
+            label.AppendLine($"{product.GetName()} ({product.GetProductId()})");
+        }
+
+        return label.ToString();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
