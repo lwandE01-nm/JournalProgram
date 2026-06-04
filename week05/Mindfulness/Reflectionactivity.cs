6388d96055589803417ca7071e21f2c4995b150a
lwandE01-nm <lwandE01@icloud.com>
2026-06-04 14:45:49 +0200

    Update Reflectionactivity.cs

--- week05/Mindfulness/Reflectionactivity.cs
+++ week05/Mindfulness/Reflectionactivity.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 public class ReflectionActivity : Activity
 {
     private List<string> _prompts = new List<string>()
@@ -14,17 +11,18 @@ public class ReflectionActivity : Activity
     private List<string> _questions = new List<string>()
     {
         "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
         "How did you get started?",
-        "What did you learn about yourself?",
         "How did you feel when it was complete?",
-        "What could you learn from this experience?"
+        "What made this time different?",
+        "What is your favorite thing about this experience?",
+        "What did you learn about yourself?",
+        "How can you remember this experience in the future?"
     };
-
-    private Random _random = new Random();
 
     public ReflectionActivity()
         : base(
-            "Reflection",
+            "Reflection Activity",
             "This activity will help you reflect on times in your life when you have shown strength and resilience.")
     {
     }
@@ -33,27 +31,24 @@ public class ReflectionActivity : Activity
     {
         DisplayStartingMessage();
 
-        string prompt = _prompts[_random.Next(_prompts.Count)];
+        Random random = new Random();
 
         Console.WriteLine("\nConsider the following prompt:");
-        Console.WriteLine($"\n--- {prompt} ---");
+        Console.WriteLine($"\n--- {_prompts[random.Next(_prompts.Count)]} ---");
 
         Console.WriteLine("\nWhen you have something in mind press Enter.");
         Console.ReadLine();
 
-        Console.WriteLine("\nReflect on the following questions:");
+        Console.WriteLine("\nNow ponder each question:");
 
         DateTime endTime = DateTime.Now.AddSeconds(_duration);
 
         while (DateTime.Now < endTime)
         {
-            string question = _questions[_random.Next(_questions.Count)];
-
-            Console.WriteLine($"\n> {question}");
+            Console.WriteLine($"\n> {_questions[random.Next(_questions.Count)]}");
             ShowSpinner(5);
         }
 
         DisplayEndingMessage();
     }
 }
-
