8112571ce3a1d04eab05e9392384104ba8849145
lwandE01-nm <lwandE01@icloud.com>
2026-06-16 20:13:37 +0200

    Add Exercise Tracking Program with activities

---
+++ week07/ExerciseTracking/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new RunningActivity(
+            new DateTime(2022, 11, 3), 30, 4.8));
+
+        activities.Add(new CyclingActivity(
+            new DateTime(2022, 11, 4), 45, 20.0));
+
+        activities.Add(new SwimmingActivity(
+            new DateTime(2022, 11, 5), 40, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
